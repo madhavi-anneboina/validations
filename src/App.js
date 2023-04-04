b80501d2d00b15701bddfc75f9f673fa53f2ba73
madhavi-anneboina <128103581+madhavi-anneboina@users.noreply.github.com>
2023-04-04 12:27:11 -0400

    validations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import React from 'react'
 
-function App() {
+const App = () => {
+  const [formdata, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+  const [formErrors, setFormErrors] = useState({})
+
+  const {name, email, password} = formdata
+
+  const chageHandler = e => {
+    setFormData({...formdata, [e.target.name]: e.target.value })
+  
+  }
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    const errors = validateForm(formdata)
+    setFormErrors(errors)
+    if(Object.keys(errors).length === 0){
+      console.log("Form is valid Submiting")
+    } else{
+      console.log("form is invalid fix the errors")
+    }
+
+  }
+
+  const validateForm = (fields) =>{
+    const errors = {};
+    if (!fields.name.trim()){
+      errors.name = "Name is required"
+    } if (!fields.email.trim()){
+      errors.email = "Email is requird"
+    } else if (!/\S+@\S+\.\S+/.test(fields.email)){
+      errors.email = "Email is invalid"
+    } if (!fields.password.trim()){
+      errors.password = "Password is Required"
+    } else if ( fields.password.trim().length < 6) {
+      errors.password = "Password must be atleast 6 char"
+    }
+    return errors;
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitHandler}>
+        <center>
+          <label htmlFor='name'> Name</label>
+          <input type="text" name="name" value={name} onChange={chageHandler} /> <br />
+          {formErrors.name && <p className="error"> {formErrors.name}</p>}
+          <label htmlFor='email'> Email</label>
+          <input type="email" name="email" value={email} onChange={chageHandler} /> <br />
+          {formErrors.email && <p className="error "> {formErrors.email}</p>}
+          <label htmlFor='password'> Password</label>
+          <input type="password" name="password" value={password} onChange={chageHandler} /> <br />
+          {formErrors.password && <p className="error"> {formErrors.password}</p>}
+          <button> Submit </button>
+
+        </center>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
